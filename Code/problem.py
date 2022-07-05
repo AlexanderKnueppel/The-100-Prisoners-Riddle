5c7ab7d45c5e532c0435fec8ab2cc179e2cad748
Alexander Knueppel <a.knueppel@tu-bs.de>
2022-07-05 22:44:51 +0200

    Add histogram and some values

--- Code/problem.py
+++ Code/problem.py
@@ -13,34 +13,43 @@
 # This short python script is an implementation of this strategy to emperically see that this is the case.
 
 import random
+import matplotlib.pyplot as plt
 
-N = 100 # Number of tries/rounds
+def OptimalStrategy(N, P):
+    failures = 0
+
+    for n in range(N):
+        drawers = random.sample(range(P), P)
+        idx = 0
+        while idx < P:
+            # Create loop
+            loop = [drawers[idx]]
+            while True:
+                next = drawers[loop[-1]]
+                drawers[loop[-1]] = -1
+                if next == loop[0]:
+                    break
+                loop.append(next)
+
+            if len(loop) > 50:
+                failures += 1
+                break
+
+            # Move idx
+            while idx < P and drawers[idx] == -1:
+                idx += 1
+    return float(1-failures/N)
+
 P = 100 # Number of prisoners
 
-failures = 0
+D = [2**x for x in range(15)]
+H = []
 
-for n in range(N):
-    drawers = random.sample(range(P), P)
-    idx = 0
-    while idx < P:
-        # Create loop
-        loop = [drawers[idx]]
-        while True:
-            next = drawers[loop[-1]]
-            drawers[loop[-1]] = -1
-            if next == loop[0]:
-                break
-            loop.append(next)
+for d in D:
+    H.append(OptimalStrategy(d, P))
 
-        if len(loop) > 50:
-            failures += 1
-            break
-
-        # Move idx
-        while idx < P and drawers[idx] == -1:
-            idx += 1
-
-print(f"Success rate: {float(1-failures/N)}")
-
-
-print(drawers)
+print(H)
+index = range(len(D))
+plt.bar(index, H, 0.5, color="blue")
+plt.xticks(index, D) # labels get centered
+plt.show()
